3fc75ddd54bcf457cd100410a6ee3fda99eaf5e0
VDineshGoud <120017407+VDineshGoud@users.noreply.github.com>
2023-02-27 16:50:21 +0530

    Add files via upload

---
+++ project2/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+st.title("Hi,:Blue[ I am V Dinesh] :curly_haired_man:")
+
+st.header("Data science inter @ innomatics :100:")
+
+st.subheader(":blue[Follow me on linkedin and Gitub] :")
+
+
+
+
+st.markdown(
+        f"""
+        <a href="https://www.linkedin.com/in/valandas-dinesh-goud-a45509234/">
+            LINKEDIN
+        </a>
+        """,
+        unsafe_allow_html=True)
+
+st.markdown(
+        f"""
+        <a href="https://github.com/VDineshGoud">
+        GITHUB
+        </a>
+        """,
+        unsafe_allow_html=True)
+
+
